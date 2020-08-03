3a8b722eb4ae7140a443d86ac911d97ff5185c3d
Bhanu Teja P <pbteja1998@gmail.com>
2020-08-03 10:15:31 +0530

    Add typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-accent-1">
+          Next.js + Tailwind CSS
+        </h1>
+        <h1 className="text-5xl text-center text-cyan-1">
+          Next.js + Tailwind CSS
+        </h1>
+        <h1 className="text-5xl text-center text-cyan-2">
+          Next.js + Tailwind CSS
+        </h1>
+        <h1 className="text-5xl text-center text-cyan-3">
+          Next.js + Tailwind CSS
+        </h1>
+        <h1 className="text-5xl text-center text-cyan-4">
+          Next.js + Tailwind CSS
+        </h1>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     extend: {
       colors: {
         'accent-1': '#333',
+        'cyan-1': 'hsl(180, 52%, 96%)',
+        'cyan-2': 'hsl(180, 31%, 95%)',
+        'cyan-3': 'hsl(180, 8%, 52%)',
+        'cyan-4': 'hsl(180, 14%, 20%)',
       },
     },
   },
